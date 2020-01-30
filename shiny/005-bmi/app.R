734ad4a4588e884030cb9183a4359244c82b162d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-01-30 23:28:52 +0700

    Create app.R

---
+++ shiny/005-bmi/app.R
@@ -0,0 +1,86 @@
+############################################
+# Data Professor                           #
+# http://youtube.com/dataprofessor         #
+# http://github.com/dataprofessor          #
+# http://facebook.com/dataprofessor        #
+# https://www.instagram.com/data.professor #
+############################################
+
+library(shiny)
+library(data.table)
+library(randomForest)
+library(shinythemes)
+
+
+####################################
+# User Interface                   #
+####################################
+ui <- fluidPage(theme = shinytheme("united"),
+                navbarPage("BMI Calculator:",
+                           
+                           tabPanel("Home",
+                                    # Input values
+                                    sidebarPanel(
+                                      HTML("<h3>Input parameters</h4>"),
+                                      sliderInput("height", label = "Height", value = 175, min = 40, max = 250),
+                                      sliderInput("weight", label = "Weight", value = 70, min = 20, max = 100),
+                                      
+                                      actionButton("submitbutton", "Submit", class = "btn btn-primary")
+                                    ),
+                                    
+                                    mainPanel(
+                                      tags$label(h3('Status/Output')), # Status/Output Text Box
+                                      verbatimTextOutput('contents'),
+                                      tableOutput('tabledata') # Results table
+                                    ) # mainPanel()
+                                    
+                           ), #tabPanel(), Home
+                           
+                           tabPanel("About", 
+                                    titlePanel("About"), 
+                                    div(includeMarkdown("about.md"), 
+                                        align="justify")
+                           ) #tabPanel(), About
+                           
+                ) # navbarPage()
+) # fluidPage()
+
+
+####################################
+# Server                           #
+####################################
+server <- function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    bmi <- input$weight/( (input$height/100) * (input$height/100) )
+    bmi <- data.frame(bmi)
+    names(bmi) <- "BMI"
+    print(bmi)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+  
+}
+
+
+####################################
+# Create Shiny App                 #
+####################################
+shinyApp(ui = ui, server = server)
