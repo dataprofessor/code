500ea8a092f8dfbf5a45e2e5ff30e1731cf824ad
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-06-07 10:14:47 +0700

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
