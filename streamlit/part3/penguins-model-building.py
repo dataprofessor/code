a558a95a36b163e9641ac3fb47476a108a003104
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-06-25 18:45:54 +0700

    Add files via upload

---
+++ streamlit/part3/penguins-model-building.py
@@ -0,0 +1,32 @@
+import pandas as pd
+penguins = pd.read_csv('penguins_cleaned.csv')
+
+# Ordinal feature encoding
+# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+df = penguins.copy()
+target = 'species'
+encode = ['sex','island']
+
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+    return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
+
+# Separating X and y
+X = df.drop('species', axis=1)
+Y = df['species']
+
+# Build random forest model
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+# Saving the model
+import pickle
+pickle.dump(clf, open('penguins_clf.pkl', 'wb'))
