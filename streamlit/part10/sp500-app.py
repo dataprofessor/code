1cb32832b3326fdfa0e2c4c61528ffd56e80776d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-10-11 14:19:59 +0700

    Update sp500-app.py

--- streamlit/part10/sp500-app.py
+++ streamlit/part10/sp500-app.py
@@ -11,7 +11,7 @@ st.title('S&P 500 App')
 st.markdown("""
 This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [Wikipedia](https://www.wikipedia.org/).
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
 """)
 
 st.sidebar.header('User Input Features')
